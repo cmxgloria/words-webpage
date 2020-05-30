3e0dc1c0c72dff99358050ba2ced731d87347e0a
gloria <cmxgloria@gmail.com>
2020-05-30 21:51:55 +1000

    created input and form in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [words, setWords] = useState([]);
+  const [url, setUrl] = useState([]);
+  // const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    wordsExistTimes(query);
+  }, [query]);
+
+  const wordsExistTimes = async (qs) => {
+    const apiUrl = "";
+    const respond = await fetch(apiUrl);
+    const data = await respond.json();
+    // console.log(data);
+    setWords(data);
+  };
+
+  const getQuery = (e) => {
+    e.preventDefault();
+    setQuery(words);
+    setWords("");
+    setQuery(url);
+    setUrl("");
+  };
+
+  // const updateSearch = (e) => {
+  //   setSearch(e.target.value);
+  // };
+  const updateWords = (e) => {
+    setWords(e.target.value);
+  };
+
+  const updateUrl = (e) => {
+    setUrl(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        src="https://circlein.com/wp-content/uploads/elementor/thumbs/CI-Landscape-Gradient-1-onirtzjrdivx2a3xb8n0t81wxk108wwio6undogxhi.png"
+        alt="logo"
+      />
+      <h1>WELCOME TO CIRCLE IN</h1>
+
+      <div className="app-content">
+        <form className="search_form" onSubmit={getQuery}>
+          <label htmlFor="">Words</label>
+          <input
+            className="word_input"
+            type="text"
+            value={words}
+            onChange={updateWords}
+          />
+          <label htmlFor="">URL</label>
+          <input
+            className="url_input"
+            type="text"
+            value={url}
+            onChange={updateUrl}
+          />
+          <button className="search_button" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
