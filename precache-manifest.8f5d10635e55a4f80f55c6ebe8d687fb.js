e3e167df5f9d7deed20f56b7b97f085a78d1efd1
gloria <cmxgloria@gmail.com>
2020-05-31 13:46:12 +1000

    Updates

---
+++ precache-manifest.8f5d10635e55a4f80f55c6ebe8d687fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b240ff86753f67a31d813cc4b412c91",
+    "url": "/words-webpage/index.html"
+  },
+  {
+    "revision": "d04e42d5ed6b9230fb38",
+    "url": "/words-webpage/static/css/main.37d769ff.chunk.css"
+  },
+  {
+    "revision": "414f46b7a3cb242945d3",
+    "url": "/words-webpage/static/js/2.77924114.chunk.js"
+  },
+  {
+    "revision": "4ddcf53b704e421eb3aee7a56997fb93",
+    "url": "/words-webpage/static/js/2.77924114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d04e42d5ed6b9230fb38",
+    "url": "/words-webpage/static/js/main.b7434323.chunk.js"
+  },
+  {
+    "revision": "c77a54f60554d7938321",
+    "url": "/words-webpage/static/js/runtime-main.4cbf8896.js"
+  }
+]);
